f19010b2efdb71cbfb19fb953392d569e6bab72b
Sean Donnellan <darkdonno@gmail.com>
2014-01-05 18:00:28 +1030

    Now converts the opcode and modifiers from being base-10 number representing "binary" to an actual base-10 integer.

--- dasm/parser/Instructions.cpp
+++ dasm/parser/Instructions.cpp
@@ -14,6 +14,29 @@
 
 #include <cassert>
 
+static int pow10(int power)
+{
+  assert(power > 0);
+  int result = 10;
+  for (--power; power > 0; --power)
+  {
+    result *= 10;
+  }
+  return result;
+}
+
+// Given a number in decimal-encoded binary, i.e, if number is 11 (so eleven
+// if treated as decimal), then this function will return 3 (in decimal).
+static int convertDecimalEncodedBinary(int number)
+{
+  int trueNumber = number & 1;
+  for (int i = 1; i < 6; ++i)
+  {
+    trueNumber += (number / pow10(i) == 1) ? (1 << i) : 0;
+  }
+  return trueNumber;
+}
+
 static dlx::assembly::instructions::InstructionMap& allInstructions()
 {
   static dlx::assembly::instructions::InstructionMap map;
@@ -28,7 +51,8 @@ dlx::assembly::instructions::all()
 
 dlx::assembly::InstructionDefinition::InstructionDefinition(
   const std::string& mnemonic, int opcode, Instruction::Format format)
-: mnemonic(mnemonic), opcode(opcode), modifier(0), format(format)
+: mnemonic(mnemonic), opcode(convertDecimalEncodedBinary(opcode)), modifier(0),
+  format(format)
 {
   // This construction shouldn't be used for register-to-register instructions.
   assert(format != Instruction::RegisterToRegister);
@@ -37,7 +61,9 @@ dlx::assembly::InstructionDefinition::InstructionDefinition(
 
 dlx::assembly::InstructionDefinition::InstructionDefinition(
   const std::string& mnemonic, int opcode, int modifier)
-: mnemonic(mnemonic), opcode(opcode), modifier(modifier),
+: mnemonic(mnemonic),
+  opcode(convertDecimalEncodedBinary(opcode)),
+  modifier(convertDecimalEncodedBinary(modifier)),
   format(Instruction::RegisterToRegister)
 {
   allInstructions().insert(std::make_pair(mnemonic, this));
